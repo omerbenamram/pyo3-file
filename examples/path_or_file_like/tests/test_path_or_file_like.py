8c03876aeb824f3dd122791c6f7c063b8d340b18
Kyle Barron <kyle@developmentseed.org>
2025-04-01 10:54:49 -0400

    Add tests

--- examples/path_or_file_like/tests/test_path_or_file_like.py
+++ examples/path_or_file_like/tests/test_path_or_file_like.py
@@ -1,14 +1,18 @@
-import pytest
 import io
 from pathlib import Path
 
-from path_or_file_like import accepts_path_or_file_like_read, accepts_file_like_write
+import pytest
+from path_or_file_like import (
+    accepts_file_like_write,
+    accepts_path_or_file_like_read,
+    name_of_file_like,
+)
 
 
 @pytest.fixture
 def small_sample() -> str:
     p = Path(__file__).parent.parent
-    return str(p.joinpath('sample.txt'))
+    return str(p.joinpath("sample.txt"))
 
 
 def test_it_reads_from_io_object(small_sample):
@@ -17,11 +21,13 @@ def test_it_reads_from_io_object(small_sample):
 
     assert accepts_path_or_file_like_read(io.BytesIO(r)) == "Hello World!"
 
+
 def test_it_reads_from_textio_object(small_sample):
     with open(small_sample, "rt") as o:
         r = o.read()
 
     assert accepts_path_or_file_like_read(io.StringIO(r)) == "Hello World!"
+
 
 def test_it_reads_from_file_backed_object(small_sample):
     with open(small_sample, "rb") as o:
@@ -35,11 +41,12 @@ def test_it_fails_on_non_file_object():
 
 def test_it_fails_when_write_returns_none():
     class FileLike:
-
         def write(self, _data):
             return None
 
-    with pytest.raises(OSError, match=r'write\(\) returned None, expected number of bytes written'):
+    with pytest.raises(
+        OSError, match=r"write\(\) returned None, expected number of bytes written"
+    ):
         accepts_file_like_write(FileLike())
 
 
@@ -53,3 +60,13 @@ def test_it_writes_to_textio_object():
     f = io.StringIO()
     accepts_file_like_write(f)
     assert f.getvalue() == "Hello, world!"
+
+
+def test_file_name_from_path(small_sample: str):
+    with open(small_sample, "rb") as o:
+        assert name_of_file_like(o) == small_sample
+
+
+def test_file_name_from_textio():
+    f = io.StringIO()
+    assert name_of_file_like(f) is None
